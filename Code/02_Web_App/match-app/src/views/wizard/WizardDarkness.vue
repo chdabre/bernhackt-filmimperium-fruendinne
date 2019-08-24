463fe38192661a2f158173e73540ae25e62d2dab
Dario Breitenstein <online@imakethings.ch>
2019-08-25 00:40:11 +0200

    wizard darkness

--- Code/02_Web_App/match-app/src/views/wizard/WizardDarkness.vue
+++ Code/02_Web_App/match-app/src/views/wizard/WizardDarkness.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="flex-grow-1 d-flex flex-column">
-    <h2>magst du es lieber hell oder dunkel?</h2>
+    <h2 class="title-darkness" :class="{'white--text': 646 * darkness * .01 > 570}">magst du es lieber hell oder dunkel?</h2>
 
     <v-container>
       <v-row>
         <v-slider
+          class="title-darkness mt-12"
           min="0"
           max="100"
           v-model="darkness"
@@ -12,6 +13,8 @@
         ></v-slider>
       </v-row>
     </v-container>
+
+    <div class="overlay" :style="`height: ${646 * darkness * .01}px;`"></div>
   </div>
 </template>
 
@@ -23,7 +26,7 @@ export default {
     }
   },
   watch: {
-    wokeness () {
+    darkness () {
       this.$store.commit('setDarkness', this.darkness)
     }
   }
@@ -31,4 +34,15 @@ export default {
 </script>
 
 <style lang="scss">
+.title-darkness {
+  z-index: 1;
+}
+.overlay {
+  background-color: rgba(0,0,0,0.7);
+  border-radius: 4px;
+  position: fixed;
+  bottom: 16px;
+  left: 0;
+  right: 0
+}
 </style>
